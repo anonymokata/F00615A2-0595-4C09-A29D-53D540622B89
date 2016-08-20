e6bf3587e02378a129e60d669f3f9bfb0689a543
anonymous <anonymous>
2016-08-20 14:36:57 -0400

    Combine serveral tests into a single one

--- unit-tests/test_conversions.c
+++ unit-tests/test_conversions.c
@@ -10,7 +10,7 @@ START_TEST(prove_check_framework_can_fail)
 }
 END_TEST
 
-START_TEST(test_roman_numeral_indexes)
+START_TEST(test_roman_numeral_index_type)
 {
     ck_assert_int_eq(0, INVALID_NUMERAL);
     ck_assert_int_eq(1, I);
@@ -29,7 +29,7 @@ START_TEST(test_roman_numeral_indexes)
 }
 END_TEST
 
-START_TEST(test_roman_numeral_values)
+START_TEST(test_roman_numeral_value_type)
 {
     ck_assert_int_eq(0, nINVALID_NUMERAL);
     ck_assert_int_eq(1, nI);
@@ -48,88 +48,23 @@ START_TEST(test_roman_numeral_values)
 }
 END_TEST
 
-START_TEST(numeral_value_will_return_nINVALID_NUMERAL_when_no_condition_is_met)
+START_TEST(numeral_value_will_return_expected_values)
 {
     ck_assert_int_eq(numeral_value(NULL), nINVALID_NUMERAL);
     ck_assert_int_eq(numeral_value(""), nINVALID_NUMERAL);
     ck_assert_int_eq(numeral_value("Z"), nINVALID_NUMERAL);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_one_when_letter_is_I)
-{
     ck_assert_int_eq(numeral_value("I"), nI);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_four_when_letter_is_I_followed_by_V)
-{
     ck_assert_int_eq(numeral_value("IV"), nIV);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_five_when_letter_is_V)
-{
     ck_assert_int_eq(numeral_value("V"), nV);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_nine_when_letter_is_I_followed_by_X)
-{
     ck_assert_int_eq(numeral_value("IX"), nIX);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_ten_when_letter_is_X)
-{
     ck_assert_int_eq(numeral_value("X"), nX);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_forty_when_letter_is_X_followed_by_L)
-{
     ck_assert_int_eq(numeral_value("XL"), nXL);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_fifty_when_letter_is_L)
-{
     ck_assert_int_eq(numeral_value("L"), nL);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_ninety_when_letter_is_X_followed_by_C)
-{
     ck_assert_int_eq(numeral_value("XC"), nXC);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_one_hundred_when_letter_is_C)
-{
     ck_assert_int_eq(numeral_value("C"), nC);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_four_hundred_when_letter_is_C_followed_by_D)
-{
     ck_assert_int_eq(numeral_value("CD"), nCD);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_five_hundred_when_letter_is_D)
-{
     ck_assert_int_eq(numeral_value("D"), nD);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_nine_hundred_when_letter_is_C_followed_by_M)
-{
     ck_assert_int_eq(numeral_value("CM"), nCM);
-}
-END_TEST
-
-START_TEST(numeral_value_will_return_one_thousand_when_letter_is_M)
-{
     ck_assert_int_eq(numeral_value("M"), nM);
 }
 END_TEST
@@ -148,22 +83,9 @@ Suite* roman_calculator_suite()
 
     TCase *tc_conversions = tcase_create("Conversion");
     tcase_add_exit_test(tc_conversions, prove_check_framework_can_fail, EXIT_FAILURE);
-    tcase_add_test(tc_conversions, test_roman_numeral_indexes);
-    tcase_add_test(tc_conversions, test_roman_numeral_values);
-    tcase_add_test(tc_conversions, numeral_value_will_return_nINVALID_NUMERAL_when_no_condition_is_met);
-    tcase_add_test(tc_conversions, numeral_value_will_return_one_when_letter_is_I);
-    tcase_add_test(tc_conversions, numeral_value_will_return_four_when_letter_is_I_followed_by_V);
-    tcase_add_test(tc_conversions, numeral_value_will_return_five_when_letter_is_V);
-    tcase_add_test(tc_conversions, numeral_value_will_return_nine_when_letter_is_I_followed_by_X);
-    tcase_add_test(tc_conversions, numeral_value_will_return_ten_when_letter_is_X);
-    tcase_add_test(tc_conversions, numeral_value_will_return_forty_when_letter_is_X_followed_by_L);
-    tcase_add_test(tc_conversions, numeral_value_will_return_fifty_when_letter_is_L);
-    tcase_add_test(tc_conversions, numeral_value_will_return_ninety_when_letter_is_X_followed_by_C);
-    tcase_add_test(tc_conversions, numeral_value_will_return_one_hundred_when_letter_is_C);
-    tcase_add_test(tc_conversions, numeral_value_will_return_four_hundred_when_letter_is_C_followed_by_D);
-    tcase_add_test(tc_conversions, numeral_value_will_return_five_hundred_when_letter_is_D);
-    tcase_add_test(tc_conversions, numeral_value_will_return_nine_hundred_when_letter_is_C_followed_by_M);
-    tcase_add_test(tc_conversions, numeral_value_will_return_one_thousand_when_letter_is_M);
+    tcase_add_test(tc_conversions, test_roman_numeral_index_type);
+    tcase_add_test(tc_conversions, test_roman_numeral_value_type);
+    tcase_add_test(tc_conversions, numeral_value_will_return_expected_values);
     tcase_add_test(tc_conversions, numeral_to_uint_will_return_INVALID_NUMERAL_when_no_condition_is_met);
     suite_add_tcase(s, tc_conversions);
     return s;
