128aa566117dfca791117d81c98cd3c1e9594a37
anonymous <anonymous>
2016-08-21 20:30:43 -0400

    Added implementation of substract function

--- src/roman_calculator.c
+++ src/roman_calculator.c
@@ -16,9 +16,18 @@ char* add(const char* numeral1, const char* numeral2)
 
 char* substract(const char* numeral1, const char* numeral2)
 {
-    if((NULL != numeral1) && (NULL == numeral2))
+    if(NULL != numeral1)
     {
-        return strdup(numeral1);
+        if(NULL == numeral2)
+        {
+            return strdup(numeral1);
+        }
+        uint16_t value1 = numeral_to_uint(numeral1);
+        uint16_t value2 = numeral_to_uint(numeral2);
+        if(value1 >= value2)
+        {
+            return uint_to_numeral(value1 - value2);
+        }
     }
     return NULL;
 }
--- unit-tests/test_roman_calculator.c
+++ unit-tests/test_roman_calculator.c
@@ -52,7 +52,22 @@ END_TEST
 
 START_TEST(substract_function_will_return_NULL_if_the_first_parameter_is_NULL)
 {
-    ck_assert_ptr_eq(NULL, substract(NULL, ""));
+    ck_assert_ptr_eq(NULL, substract(NULL, "I"));
+}
+END_TEST
+
+START_TEST(substract_function_will_return_NULL_if_the_first_parameter_is_not_greater_or_equal_than_second_one)
+{
+    ck_assert_ptr_eq(NULL, substract("I", "II"));
+}
+END_TEST
+
+START_TEST(substract_function_will_return_the_substraction_of_the_second_parameter_from_the_first_one)
+{
+    char* result = substract("II", "I");
+
+    ck_assert_str_eq(result, "I");
+    free(result);
 }
 END_TEST
 
@@ -65,5 +80,7 @@ void add_roman_calculator_test_case(Suite *s)
     tcase_add_test(tc_roman_calculator, substract_function_will_return_NULL_when_both_function_parameters_are_NULL);
     tcase_add_test(tc_roman_calculator, substract_function_will_return_the_first_parameter_if_the_other_one_is_NULL);
     tcase_add_test(tc_roman_calculator, substract_function_will_return_NULL_if_the_first_parameter_is_NULL);
+    tcase_add_test(tc_roman_calculator, substract_function_will_return_NULL_if_the_first_parameter_is_not_greater_or_equal_than_second_one);
+    tcase_add_test(tc_roman_calculator, substract_function_will_return_the_substraction_of_the_second_parameter_from_the_first_one);
     suite_add_tcase(s, tc_roman_calculator);
 }
