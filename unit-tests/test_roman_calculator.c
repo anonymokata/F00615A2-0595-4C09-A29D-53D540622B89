31054571de3128714cf5005d8912dd7b044d3318
anonymous <anonymous>
2016-08-25 12:11:58 -0400

    Added test to show add function will return an empty string when result wrapps down to 0

--- unit-tests/test_roman_calculator.c
+++ unit-tests/test_roman_calculator.c
@@ -50,6 +50,15 @@ START_TEST(add_function_will_return_wrapped_around_values_when_result_is_greater
 }
 END_TEST
 
+START_TEST(add_function_will_return_an_empty_string_when_result_wrapps_down_to_0)
+{
+    char* result = add("MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM", "DXXXVI");
+
+    ck_assert_str_eq(result, "");
+    free(result);
+}
+END_TEST
+
 START_TEST(substract_function_will_return_NULL_when_a_function_parameter_is_not_a_valid_roman_numeral)
 {
     ck_assert_ptr_eq(NULL, substract(NULL, NULL));
@@ -92,6 +101,7 @@ void add_roman_calculator_test_case(Suite *s)
     tcase_add_test(tc_roman_calculator, add_function_will_return_NULL_when_a_function_parameter_is_not_a_valid_roman_numeral);
     tcase_add_test(tc_roman_calculator, add_function_will_return_the_addition_of_two_roman_numerals);
     tcase_add_test(tc_roman_calculator, add_function_will_return_wrapped_around_values_when_result_is_greater_than_65535);
+    tcase_add_test(tc_roman_calculator, add_function_will_return_an_empty_string_when_result_wrapps_down_to_0);
     tcase_add_test(tc_roman_calculator, substract_function_will_return_NULL_when_a_function_parameter_is_not_a_valid_roman_numeral);
     tcase_add_test(tc_roman_calculator, substract_function_will_return_NULL_if_the_first_parameter_is_not_greater_or_equal_than_second_one);
     tcase_add_test(tc_roman_calculator, substract_function_will_return_the_substraction_of_the_second_parameter_from_the_first_one);
