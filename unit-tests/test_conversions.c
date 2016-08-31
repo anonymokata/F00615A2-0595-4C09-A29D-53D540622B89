378a1a16500f586f2b1aba306801a22e81ed9ad7
anonymous <anonymous>
2016-08-31 08:35:26 -0400

    Added test to show numerals I, X, or C can only be used once for substraction

--- unit-tests/test_conversions.c
+++ unit-tests/test_conversions.c
@@ -465,6 +465,17 @@ START_TEST(is_valid_roman_numeral_function_will_return_false_if_numerals_I_X_or_
 }
 END_TEST
 
+START_TEST(is_valid_roman_numeral_function_will_return_false_if_numerals_I_X_or_C_are_used_more_than_once_for_substraction)
+{
+    ck_assert(!is_valid_roman_numeral("IIV"));
+    ck_assert(!is_valid_roman_numeral("IIIX"));
+    ck_assert(!is_valid_roman_numeral("XXL"));
+    ck_assert(!is_valid_roman_numeral("XXXC"));
+    ck_assert(!is_valid_roman_numeral("CCD"));
+    ck_assert(!is_valid_roman_numeral("CCCM"));
+}
+END_TEST
+
 Suite* roman_calculator_suite()
 {
     Suite *s = suite_create("Roman Calculator");
@@ -494,6 +505,7 @@ Suite* roman_calculator_suite()
     tcase_add_test(tc_conversions, is_valid_roman_numeral_function_will_return_true_if_numerals_I_X_or_C_are_repeated_less_than_4_times);
     tcase_add_test(tc_conversions, is_valid_roman_numeral_function_will_return_false_if_numerals_V_L_or_D_are_used_for_substraction);
     tcase_add_test(tc_conversions, is_valid_roman_numeral_function_will_return_false_if_numerals_I_X_or_C_substraction_is_forbidden);
+    tcase_add_test(tc_conversions, is_valid_roman_numeral_function_will_return_false_if_numerals_I_X_or_C_are_used_more_than_once_for_substraction);
     suite_add_tcase(s, tc_conversions);
     return s;
 }
