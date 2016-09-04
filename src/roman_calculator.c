b8ca23ac6a7929d2bbfafc7ff2e5fa0f587b095b
anonymous <anonymous>
2016-09-04 14:05:52 -0400

    Added more tests around values returned by add function

--- src/roman_calculator.c
+++ src/roman_calculator.c
@@ -6,7 +6,7 @@
 
 char* add(const char* numeral1, const char* numeral2, char result[16])
 {
-    if(is_valid_roman_numeral(numeral1) && is_valid_roman_numeral(numeral2))
+    if(is_valid_roman_numeral(numeral1) && is_valid_roman_numeral(numeral2) && (NULL != result))
     {
         return uint_to_numeral(numeral_to_uint(numeral1) + numeral_to_uint(numeral2));
     }
--- unit-tests/test_roman_calculator.c
+++ unit-tests/test_roman_calculator.c
@@ -3,69 +3,104 @@
 
 #include <stdlib.h>
 
+START_TEST(add_function_will_return_NULL_when_any_function_parameter_is_NULL)
+{
+    char result[16];
+
+    ck_assert_ptr_eq(add(NULL, NULL, NULL), NULL);
+    ck_assert_ptr_eq(add("I", "I", NULL), NULL);
+    ck_assert_ptr_eq(add("I", NULL, result), NULL);
+    ck_assert_ptr_eq(add(NULL, "I", result), NULL);
+}
+END_TEST
+
+START_TEST(add_function_will_return_a_pointer_to_the_result_variable_when_function_parameters_are_not_NULL)
+{
+    char result[16];
+
+    ck_assert_ptr_eq(add("I", "I", result), result);
+}
+END_TEST
+
 START_TEST(add_function_will_return_an_empty_string_when_a_function_parameter_is_not_a_valid_roman_numeral)
 {
-    char result[16] = "invalid";
+    char result[16];
 
     strcpy(result, "invalid");
-    ck_assert_str_eq("", add("", "", result));
+    add("", "", result);
+    ck_assert_str_eq("", result);
+
     strcpy(result, "invalid");
-    ck_assert_str_eq("", add("I", "", result));
+    add("I", "", result);
+    ck_assert_str_eq("", result);
+    
     strcpy(result, "invalid");
-    ck_assert_str_eq("", add("", "I", result));
+    add("", "I", result);
+    ck_assert_str_eq("", result);
+    
     strcpy(result, "invalid");
-    ck_assert_str_eq("", add("b", "I", result));
+    add("b", "I", result);
+    ck_assert_str_eq("", result);
+
     strcpy(result, "invalid");
-    ck_assert_str_eq("", add("VX", "I", result));
+    add("VX", "I", result);
+    ck_assert_str_eq("", result);
+    
     strcpy(result, "invalid");
-    ck_assert_str_eq("", add("V", "CCCC", result));
+    add("V", "CCCC", result);
+    ck_assert_str_eq("", result);
+    
     strcpy(result, "invalid");
-    ck_assert_str_eq("", add("V", "IM", result));
+    add("V", "IM", result);
+    ck_assert_str_eq("", result);
+    
     strcpy(result, "invalid");
-    ck_assert_str_eq("", add("VV", "M", result));
+    add("VV", "M", result);
+    ck_assert_str_eq("", result);
 }
 END_TEST
 
 START_TEST(add_function_will_return_the_addition_of_two_roman_numerals)
 {
-    char* result = add("I", "I");
+    char result[16];
 
+    strcpy(result, "invalid");
+    add("I", "I", result);
     ck_assert_str_eq(result, "II");
-    free(result);
 
-    result = add("II", "II");
+    strcpy(result, "invalid");
+    add("II", "II", result);
     ck_assert_str_eq(result, "IV");
-    free(result);
 
-    result = add("IX", "I");
+    strcpy(result, "invalid");
+    add("IX", "I", result);
     ck_assert_str_eq(result, "X");
-    free(result);
 
-    result = add("V", "V");
+    strcpy(result, "invalid");
+    add("V", "V", result);
     ck_assert_str_eq(result, "X");
-    free(result);
 
-    result = add("MMMCMXCIX", "II");
+    strcpy(result, "invalid");
+    add("MMMCMXCIX", "II", result);
     ck_assert_str_eq(result, "I");
-    free(result);
 }
 END_TEST
 
 START_TEST(add_function_will_return_wrapped_around_values_when_result_is_greater_than_3999)
 {
-    char* result = add("MMMCMXCIX", "II");
+    char result[16] = "invalid";
 
+    add("MMMCMXCIX", "II", result);
     ck_assert_str_eq(result, "I");
-    free(result);
 }
 END_TEST
 
 START_TEST(add_function_will_return_an_empty_string_when_result_wrapps_down_to_0)
 {
-    char* result = add("MMMCMXCIX", "I");
+    char result[16] = "invalid";
 
+    add("MMMCMXCIX", "I", result);
     ck_assert_str_eq(result, "");
-    free(result);
 }
 END_TEST
 
@@ -111,6 +146,8 @@ END_TEST
 void add_roman_calculator_test_case(Suite *s)
 {
     TCase *tc_roman_calculator = tcase_create("Calculator");
+    tcase_add_test(tc_roman_calculator, add_function_will_return_NULL_when_any_function_parameter_is_NULL);
+    tcase_add_test(tc_roman_calculator, add_function_will_return_a_pointer_to_the_result_variable_when_function_parameters_are_not_NULL);
     tcase_add_test(tc_roman_calculator, add_function_will_return_an_empty_string_when_a_function_parameter_is_not_a_valid_roman_numeral);
     tcase_add_test(tc_roman_calculator, add_function_will_return_the_addition_of_two_roman_numerals);
     tcase_add_test(tc_roman_calculator, add_function_will_return_wrapped_around_values_when_result_is_greater_than_3999);
