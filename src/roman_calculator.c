de89cc42b1c7b6876b4828dac53e9acc22e7611b
anonymous <anonymous>
2016-09-04 15:27:48 -0400

    Refactored check for valid function parameters to a separate private function

--- src/roman_calculator.c
+++ src/roman_calculator.c
@@ -4,9 +4,18 @@
 
 #include <stdlib.h>
 
-char* add(const char* numeral1, const char* numeral2, char result[16])
+static bool function_paramenters_are_valid(const char* numeral1, const char* numeral2, char result[16])
 {
     if(is_valid_roman_numeral(numeral1) && is_valid_roman_numeral(numeral2) && (NULL != result))
+    {
+        return true;
+    }
+    return false;
+}
+
+char* add(const char* numeral1, const char* numeral2, char result[16])
+{
+    if(function_paramenters_are_valid(numeral1, numeral2, result))
     {
         return uint_to_numeral(numeral_to_uint(numeral1) + numeral_to_uint(numeral2), result);
     }
@@ -15,7 +24,7 @@ char* add(const char* numeral1, const char* numeral2, char result[16])
 
 char* subtract(const char* numeral1, const char* numeral2, char result[16])
 {
-    if(is_valid_roman_numeral(numeral1) && is_valid_roman_numeral(numeral2) && (NULL != result))
+    if(function_paramenters_are_valid(numeral1, numeral2, result))
     {
         uint16_t value1 = numeral_to_uint(numeral1);
         uint16_t value2 = numeral_to_uint(numeral2);
